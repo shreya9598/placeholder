1dc2a82f25051ec83a4fb7fbb5bee1951cdd5917
shreya9598 <84185050+shreya9598@users.noreply.github.com>
2024-05-01 23:32:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use("/users", userRouter)
 app.use("/images", imageRouter)
 
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
     console.log("server is listenign at "+ PORT)
 })
